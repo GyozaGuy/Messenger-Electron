ebb59de09d958d4143abb5e0c86eae5d9b2db2f8
Steven Stromberg <steven.stromberg@intel.com>
2015-12-15 17:51:28 -0700

    First commit to development

--- js/browser.js
+++ js/browser.js
@@ -1,15 +1,57 @@
 'use strict';
 
-const IPC = require('electron').ipcRenderer;
-const NativeNotification = Notification;
+var ipc = require('electron').ipcRenderer;
+var NativeNotification = Notification;
 
-Notification = function(title, options) {
-  const notification = new NativeNotification(title, options);
+function fixChatBoxSize() {
+  console.log('started event stuff');
+  // var box = document.getElementsByClassName('_209g')[0];
+  // box.firstChild.innerHTML = '<br data-text="true">';
+  // console.log(chatbox);
+  // var eventObj = document.createEvent('Events');
+  // console.log(eventObj);
+  // if (eventObj) {
+  //   eventObj.initEvent('keydown', true, true);
+  // }
+  // eventObj.keyCode = 8;
+  // eventObj.which = 8;
+  // console.log(chatbox.dispatchEvent(eventObj));
+  var box = document.getElementsByClassName('_54-z')[0];
+  var Podium = {};
+  Podium.keydown = function(k) {
+    console.log('in Podium');
+    var newEvent = document.createEvent('KeyboardEvent');
+    Object.defineProperty(newEvent, 'keyCode', {
+      get : function() {
+        return this.keyCodeVal;
+      }
+    });
+    Object.defineProperty(newEvent, 'which', {
+      get : function() {
+        return this.keyCodeVal;
+      }
+    });
+    newEvent.initKeyboardEvent('keydown', true, true, document.defaultView, false, false, false, false, k, k);
+    newEvent.keyCodeVal = k;
+    if (newEvent.keyCode !== k) {
+      console.log('keyCode mismatch ' + newEvent.keyCode + '(' + newEvent.which + ')');
+    }
+    console.log('dispatching...');
+    console.log(newEvent.keyCode);
+    box.dispatchEvent(newEvent);
+    console.log('dispatched');
+  }
+  Podium.keydown(8);
+  console.log('passed event stuff');
+}
 
-  IPC.send('change-icon');
+Notification = (title, options) => {
+  var notification = new NativeNotification(title, options);
+
+  ipc.send('change-icon');
 
   notification.addEventListener('click', () => {
-    IPC.send('notification-click');
+    ipc.send('notification-click');
   });
 
   return notification;
@@ -19,12 +61,17 @@ Notification.prototype = NativeNotification.prototype;
 Notification.permission = NativeNotification.permission;
 Notification.requestPermission = NativeNotification.requestPermission.bind(Notification);
 
-IPC.on('loaded', () => {
+ipc.on('loaded', () => {
+  // fixChatBoxSize();
   var chatbox = document.getElementsByClassName('_54-z')[0];
-  chatbox.innerHTML = '';
-  chatbox.onkeypress = function(e) {
+  chatbox.onkeypress = (e) => {
     if (e.keyCode == 13) {
-      chatbox.innerHTML = '';
+      fixChatBoxSize();
     }
   };
+  // var subchatbox = document.getElementsByClassName('_209g')[0].firstChild;
+  // console.log(subchatbox);
+  // subchatbox.onkeypress = (e) => {
+  //   console.log(e.keyCode);
+  // };
 });
